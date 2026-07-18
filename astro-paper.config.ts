be55684ffae3dfd90444c00304c2ff766ae43a4c
wdpmlgratis <wdpmlgratis@gmail.com>
2026-07-18 10:13:38 +0700

    update

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,18 +2,18 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://wdpmlgratis.my.id/",
+    title: "WDP ML GRATIS",
+    description: "WDP ML GRATIS | Ikuti Giveaway WDP ML gratis disini!",
+    author: "WDP ML GRATIS",
+    profile: "https://wdpmlgratis.my.id/",
     ogImage: "default-og.jpg",
     lang: "en",
     timezone: "Asia/Bangkok",
     dir: "ltr",
   },
   posts: {
-    perPage: 4,
+    perPage: 99,
     perIndex: 4,
     scheduledPostMargin: 15 * 60 * 1000,
   },
@@ -24,15 +24,15 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      url: "https://github.com/wdpmlgratis/wdpmlgratis.github.io",
     },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
+    { name: "github",   url: "https://github.com/wdpmlgratis" },
     { name: "x",        url: "https://x.com/username" },
     { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "mail",     url: "mailto:wdpmlgratis@gmail.com" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
